469d2c222601c3e6607023f976ed05135b61cbed
Abhishekyadav3824 <aradaabhishek@gmail.com>
2025-09-17 13:27:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
 //your JS code here. If required.
+      const inputs = document.querySelectorAll(".code-container");
+
+      inputs.forEach((input, index) => {
+        input.addEventListener("input", (e) => {
+          const value = e.target.value;
+          if (value.length > 1) {
+            e.target.value = value.charAt(0); // allow only 1 digit
+          }
+          if (value && index < inputs.length - 1) {
+            inputs[index + 1].focus();
+          }
+        });
+
+        input.addEventListener("keydown", (e) => {
+          if (e.key === "Backspace") {
+            if (input.value === "" && index > 0) {
+              inputs[index - 1].focus();
+              inputs[index - 1].value = "";
+            }
+          }
+        });
+      });
+
+      // Auto focus first box on load
+      inputs[0].focus();
